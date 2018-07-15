47f8e2ff087a54c46125a7b99f6ec19aa1663087
Trey <treyw1850@gmail.com>
2018-07-15 01:52:54 -0400

    worked on look of account component

--- client/src/containers/account/account.tsx
+++ client/src/containers/account/account.tsx
@@ -2,33 +2,66 @@
 import * as React from "react";
 
 // // import components
-import { 
-    Input,
+import {
+  Col, Input, Row,
 } from "../../components";
 
 interface IState {
-    hasInput: boolean
+  hasInput: boolean
 }
 
 import './style.css';
 
 
 class Account extends React.Component {
- public state: IState = {
-     hasInput: true,
+  public state: IState = {
+    hasInput: true,
   }
 
 
 
- 
+
   public render() {
     return (
-      <div className='Account'>
-        account here
-        <Input 
-        itype='input'
-        label='yea'
-        />
+      <div className='card Account'>
+        <Row>
+          <span className='Account_Name'>Warrick, Trey</span>
+          <span className='Account_Number'>8949504</span>
+          <span className="Account_Delete">X</span>
+        </Row>
+
+        <Row>
+
+          <div className="Account_Row-Space">
+            <Col size="md-5">
+              <div className='Account_Balance'>
+                Balance: <span id='Balance'>$1,678.98</span>
+              </div>
+            </Col>
+
+
+
+            <Col size="md-7">          
+            <Row>             
+              <Input
+                itype='input'
+                label='Add Funds'
+              />
+              <button className="Account_Add-Funds btn">+</button>
+            </Row>
+
+            <Row>                
+              <Input
+                itype='input'
+                label='Subtract Funds'
+              />
+              <button className="Account_Subtract-Funds btn">-</button>
+            </Row>
+            </Col>
+          </div>
+        </Row>
+
+
       </div>
     );
   }
